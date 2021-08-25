dca34bc7b79b02b062ba6c6f33f7086b1176ecd3
Xpose Protocol <88667735+XPSProtocol@users.noreply.github.com>
2021-08-25 16:45:49 +0530

    Update 2_initial_migration.js
    Token and contract name change

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,11 +1,11 @@
-const XPSToken = artifacts.require("XPSToken");
+const XposeProtocol = artifacts.require("XposeProtocol");
 
 module.exports = function (deployer) {
-  deployer.deploy(XPSToken,
+  deployer.deploy(XposeProtocol,
     'Xpose Protocol',                                   // string memory contractName,
-    'XPS',                                              // string memory contractSymbol,
+    'XP',                                              // string memory contractSymbol,
     9,                                                  // uint8 contractDecimals,
-    '30000000000000000000',                             // uint256 initialSupply,
+    '10186921194000000000',                             // uint256 initialSupply,
     '0x8B991B7B55BDe2F96F239bC1704bBB735b42C4Af',       // address payable initialMarketingPoolWallet,
     '0x87eb865Bbc69645d81a5F99f8fbC081faB5cc8e3',       // address payable initialCommunityRewardPoolWallet,
     '0xA5beA4B931AB6345266b0FB0F3D7608BbE51a689',       // address routerAddress,
